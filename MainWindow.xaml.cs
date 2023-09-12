0874613c7f3afb6fa513cf4c716fd8dcb01461e5
M153 <wanchatgame@gmail.com>
2023-09-12 22:18:23 +0700

    Realtime Database
    Firebase
    - Get
    - Push

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +27,37 @@ namespace CSharpAPI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void FirebaseConnectionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            IFirebaseConfig config = new FirebaseConfig
+            {
+                AuthSecret = "###",
+                BasePath = "https://json.firebasedatabase.app/"
+            };
+
+            IFirebaseClient client = new FirebaseClient(config);
+
+            FirebaseResponse response = await client.GetAsync("FirstName");
+            string fullname = response.ResultAs<string>();
+            MessageBox.Show(fullname);
+
+            SetDataToFirebase(client);
+        }
+
+        //เรียกใช้จาก IFirebaseClient client
+        private async void SetDataToFirebase(IFirebaseClient client)
+        {
+            var customer = new Customer
+            {
+                FirstName = "Kaka",
+                LastName = "Yaya",
+                Age = 18
+            };
+            SetResponse response = await client.SetAsync("customer/set", customer); //กำหนด path
+            Customer customer1 = response.ResultAs<Customer>();
+            MessageBox.Show(customer1.FirstName);
         }
     }
 }
